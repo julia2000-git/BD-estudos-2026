1306b05fb57905c0b4d8282e1f8c89500c631d33
julia2000-git <juliajs.costa@gmail.com>
2026-04-28 21:19:27 -0300

    feat: cria tabelas com o ODBC #222

--- 20261/tarefas/tarefa02/connection.py
+++ 20261/tarefas/tarefa02/connection.py
@@ -1,16 +1,33 @@
 import pyodbc
 
+# Iniciando a conexão com o BD
+conn = pyodbc.connect("DSN=PostgresAtvODBC;") #nome do DSN foi: PostgresAtvODBC
+# Criação do cursor para executar os comandos
+cursor = conn.cursor()
+
 try:
-    # O nome aqui deve ser o que você escreveu em "Data Source"
-    conn = pyodbc.connect("DSN=PostgresAtvODBC;") #nome do DSN foi: PostgresAtvODBC
-    print("Sucesso! Python e PostgreSQL conectados via ODBC.")
+    
+    # Etapa 1: vamos fazer a criação das tabelas
+    with open('esquema_create_tables.sql', 'r', encoding='utf-8') as arq_create:
+        sql_criar_tabelas = arq_create.read()
+
+    cursor.execute(sql_criar_tabelas) # cursor executa a função
+    conn.commit() # salva a criação
+
+    print("Sucesso! As tabelas da atividade foram criadas1")
+
+    # Etapa 2: vamos inserir os dados nas tabelas
     
     # Criar um cursor para executar comandos SQL
-    cursor = conn.cursor()
-    cursor.execute("SELECT version();")
-    row = cursor.fetchone()
-    print(f"Versão do Banco: {row[0]}")
+    #cursor = conn.cursor()
+    #cursor.execute("SELECT version();")
+    #row = cursor.fetchone()
+    #print(f"Versão do Banco: {row[0]}")
     
-    conn.close()
 except Exception as e:
     print(f"Erro na conexão: {e}")
+
+finally:
+    # 2. Fecha tudo ao final
+    cursor.close()
+    conn.close()
