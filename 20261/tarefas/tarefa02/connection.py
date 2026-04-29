dbba1bab4651233819a483ddc9316b49ac1fd812
julia2000-git <juliajs.costa@gmail.com>
2026-04-28 22:54:42 -0300

    feat: resolve itens b) e c) da uqestão 5

--- 20261/tarefas/tarefa02/connection.py
+++ 20261/tarefas/tarefa02/connection.py
@@ -35,16 +35,58 @@ try:
     # Questão 5 da Tarefa - ODBC e ORM
     # item a. Inserir uma atividade em algum projeto
 
+    descricao_atv = 'BD - Atividade 4'  # declaração de variáveis
+    cod_projeto = 3
+    data_in = '2026-03-25'
+    data_f = '2026-04-28'
+
     sql_inserir_atv_proj = """
         INSERT INTO atividade (descricao, projeto, data_inicio, data_fim)
-        values
-        ('BD - Atividade 4', 3, '2026-03-25', '2026-04-28');
+        VALUES
+        (?, ?, ?, ?);
         """
-    cursor.execute(sql_inserir_atv_proj)
+        
+    cursor.execute(sql_inserir_atv_proj, (descricao_atv, cod_projeto, data_in, data_f))
     conn.commit()
     print("A atividade foi inserida em um projeto!")
-
     
+    # ----------------------------------------------
+    # Item b. Atualizar o líder de algum projeto
+
+    sql_atualizar_lider = """
+        UPDATE projeto SET responsavel = 14 WHERE nome LIKE 'Monitoria';
+        """
+    
+    cursor.execute(sql_atualizar_lider)
+    conn.commit()
+    print("Líder de projeto atualizado!")
+
+    # ----------------------------------------------
+    # Item c. Listar todos os projetos e suas atividades
+
+    sql_listar_proj_atv = """
+        SELECT p.codigo, p.nome as nome_proj, p.descricao as descricao_proj, p.depto as depto_proj, 
+        a.descricao as nome_atvd, a.data_inicio as inicio_atvd, a.data_fim as fim_atvd 
+        FROM projeto p JOIN atividade a ON p.codigo = a.projeto
+        GROUP BY p.codigo, a.descricao, a.data_inicio, a.data_fim, p.nome
+        ORDER BY p.nome ASC;
+        """
+
+    cursor.execute(sql_listar_proj_atv)
+    conn.commit()
+
+    print("Projetos e suas atividades listados!")
+
+    # verificando se funcionou
+    linhas = cursor.fetchall()
+
+    if not linhas:
+        print("A consulta executou, mas a tabela está vazia.")
+    else:
+        print(f"Sucesso! Foram encontradas {len(linhas)} linhas:")
+        for linha in linhas:
+            print(linha)
+
 except Exception as e:
     print(f"Erro na conexão: {e}")
 
